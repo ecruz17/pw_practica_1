2866359b5f4192946135fcf239cfd5027afb6b83
ecruz17 <ecruz17@ucol.mx>
2022-03-16 18:25:12 -0600

    implemented practica 7

--- first_class/emisor/app.js
+++ first_class/emisor/app.js
@@ -1,19 +1,24 @@
-//import Emitter from "./emitter.js";
-var Emitter = require('./emitter.js');
+// var Emitter = require('./emitter.js');
+var Emitter = require('events'); //se cambió el módulo emisor creado por nosotros por el módulo events propio de node
 var emtr = new Emitter();
+var config = require('./config.js');
 
-emtr.on('greet', () => {
-    console.log('Alguien dijo hola desde algún lado');
-});
+// emtr.on('greet', () => {
+//     console.log('Alguien dijo hola desde algún lado');
+// }); versión vieja
 
-emtr.on('greet', () => {
+emtr.on(config.events.GREET, () => {
+    console.log('Alguien dijo hola desde algún lado'); 
+}); //la función modificada para llamar a greet desde el archivo config y evitar errores de typo
+
+emtr.on(config.events.GREET, () => {
     console.log('Acaba de suceder un saludazo');
 });
 
 console.log('Hola!');
 emtr.emit('greet'); //se ejecutan ambas funciones ya que ambas se llaman con 'greet'
 
-emtr.on('jump', () => {
+emtr.on(config.events.JUMP, () => {
     console.log('Alguien brincó!!');
 });
 
