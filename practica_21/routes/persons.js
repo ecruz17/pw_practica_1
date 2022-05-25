0d726d16f51e4a5975b4c709ffc7034d265f0e7c
ecruz17 <ecruz17@ucol.mx>
2022-05-25 08:41:09 -0500

    implemented practica 21

---
+++ practica_21/routes/persons.js
@@ -0,0 +1,33 @@
+const express = require('express'); //inyectamos express
+const router = express.Router(); //se instancia un nuevo Router de express
+const mongoose = require('../node_modules/mongoose'); //inyectamos moongose
+
+let Person = require('../models/person'); //inyectamos modelo Person que creamos anteriormente
+
+router.get('/persons', function(req, res, next) { //se agrega la ruta Person con el método GET
+    Person.find(function(err, person){ //se crea método para encontrar a la persona o regresar un error
+        if(err) () => next(err);
+        //res.json(person);
+        res.render('personsIndex', {person}); //se renderiza la vista con el index de la persona
+    });
+});
+
+router.get('/person', function(req, res) { //ruta con la vista para tomar los valores de la persona
+    res.render('person');
+});
+
+router.get('/main', function(req, res) { //ruta con la vista main
+    res.render('main');
+});
+
+router.post('/addPerson', function(req, res) { //nueva ruta POST para agregar datos a la base
+    const myPerson = new Person({ //crea la entidad person con sus atributos
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        tipoSangre: req.body.tipoSangre,
+        nss: req.body.nss,
+    });
+    myPerson.save(); //guarda la entidad en la base de datos
+});
+
+module.exports = router;
